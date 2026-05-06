1b844c17ca015883ba21651814560403d3efd2aa
catalinamaid <catalinamaidana18@gmail.com>
2026-05-06 15:48:09 -0300

    Ruteo

--- routes/web.php
+++ routes/web.php
@@ -5,11 +5,9 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ContactoController;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('prueba-home');
 });
-Route::get('/sobre-mi', function () {
-return view('sobre-mi');
-});
+
 Route::get('/contacto', function () {
 return view('contacto');
 })->name('contacto');
