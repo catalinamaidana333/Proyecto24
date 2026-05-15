92fb8fa41f53c2e670e515a710f53f4707911414
catalinamaid <catalinamaidana18@gmail.com>
2026-05-15 16:16:15 -0300

    modelos

---
+++ app/Models/Usuario.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+
+
+class Usuario extends Authenticatable {
+    use HasFactory, Notifiable, SoftDeletes;
+    protected $table = 'usuarios';
+    protected $fillable = ['nombre', 'email', 'password', 'rol_id'];
+    protected $hidden = ['password', 'remember_token'];
+    
+    protected function casts(): array {
+        return ['password' => 'hashed']; // Hashea automáticamente la clave
+    }
+
+    public function rol() {
+        return $this->belongsTo(Rol::class, 'rol_id');
+    }
+}
