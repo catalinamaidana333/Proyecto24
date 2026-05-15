5b94a5427c80eb856e47f73236d69d1ccf7ff598
catalinamaid <catalinamaidana18@gmail.com>
2026-05-15 16:17:14 -0300

    migrations

---
+++ database/migrations/2026_05_15_165821_create_usuarios_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('usuarios', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            
+$table->string('nombre');
+$table->string('email')->unique();
+$table->string('password'); 
+$table->foreignId('rol_id')->constrained('roles')->onDelete('restrict'); // Relación con roles
+$table->rememberToken();
+
+$table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('usuarios');
+    }
+};
