b019b377979786bbf1bba78cd5801076c1c7af3d
catalinamaid <catalinamaidana18@gmail.com>
2026-07-05 12:04:18 -0300

    revert links footer
    probando

--- resources/views/prueba-home.blade.php
+++ resources/views/prueba-home.blade.php
@@ -343,8 +343,8 @@
 
     
 <!-- Bootstrap JS -->
- <link rel="stylesheet" href="{{ asset('vendor/bootstrap/css/bootstrap.min.css') }}">
+ <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 
-<script src="{{ asset('vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
 </body>
 </html>
